565bd6e4512568ec26ee9761a6f303b0d8a4c6c9
Mike D <mike.d.hoore@telenet.be>
2019-12-01 20:32:02 +0100

    CleanUp Decryptor.js

--- src/Decryptor.js
+++ src/Decryptor.js
@@ -1,12 +1,23 @@
 const nacl = require('libsodium-wrappers')
-module.exports = async (key) => {
-    await nacl.ready
+
+let myKey = null;
+
+function setKey(key){
     if(!key){
         throw 'no key'
+    } else {
+        myKey = key
     }
+ }
+
+module.exports = async (key) => {
+    await nacl.ready
+
+    setKey(key);
+
     return Object.freeze({
         decrypt: (ciphertext, nonce) =>{
-         return nacl.crypto_secretbox_open_easy(ciphertext,nonce,key);
+         return nacl.crypto_secretbox_open_easy(ciphertext,nonce,myKey);
          }
      })
  }
