a9d88b304aa5a4d1e3646123976802b3c96fd3ed
Mike D <mike.d.hoore@telenet.be>
2019-12-01 20:54:01 +0100

    Pass 5/9 tests

--- src/SecureSessionPeer.js
+++ src/SecureSessionPeer.js
@@ -1,5 +1,28 @@
 const nacl = require('libsodium-wrappers');
-modules.export = async (peer) => {
+const Decryptor = require('./Decryptor');
+const Encryptor = require('./Encryptor');
+
+let keyPair = null;
+
+function createKeyPair(){
+    keyPair = nacl.crypto_sign_keypair();
+}
+
+module.exports = async (peer) => {
     await nacl.ready;
 
+    createKeyPair();
+
+    decryptor = await Decryptor(keyPair.publicKey);
+    encryptor = await Encryptor(keyPair.publicKey);
+
+    return Object.freeze({
+        publicKey: keyPair.publicKey,
+        encrypt: (msg) => {
+            return encryptor.encrypt(msg);
+        },
+        decrypt: (ciphertext, nonce) => {
+            return decryptor.decrypt(ciphertext, nonce);
+        }
+    });
 };
